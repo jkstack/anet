e4aae49691dc8f096a2fc93c6a1e50d0dadae224
lwch <lwch748@gmail.com>
2022-06-27 16:11:21 +0800

    修改注释

--- plugin.go
+++ plugin.go
@@ -7,5 +7,5 @@ type PluginInfo struct {
 	Name    string         `json:"name"`    // 插件名称
 	Version string         `json:"version"` // 插件版本号
 	MD5     [md5.Size]byte `json:"md5"`     // 插件文件的md5
-	URI     string         `json:"uri"`     // 插件下载的url
+	URI     string         `json:"uri"`     // 插件下载的uri
 }
